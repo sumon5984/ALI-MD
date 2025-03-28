451f7d800913f4227606120fc0ee3de864d425aa
sumon5984 <sbot7603@gmail.com>
2025-03-28 15:18:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const {
   
   //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
-if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+if(!config.SESSION_ID) return console.log('lNZUWS6C#3h7OPq_J0rahblI1pPOdUwuIIR_az1WEKET_xljcuQU')
 const sessdata = config.SESSION_ID.replace("ALI-MD~", '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
