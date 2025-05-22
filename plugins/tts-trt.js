a71aaf0eb5fddbaa4dbcfd93442770268129b2ed
sumon5984 <sbot7603@gmail.com>
2025-05-22 17:22:03 +0530

    Update tts-trt.js

--- plugins/tts-trt.js
+++ plugins/tts-trt.js
@@ -42,16 +42,16 @@ async (conn, mek, m, { from, q, reply }) => {
 //____________________________TTS___________________________
 cmd({
     pattern: "tts",
-    desc: "download songs",
+    desc: "download songs,t",
     category: "download",
-    react: "👧",
+    react: "",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 if(!q) return reply("Need some text.")
     const url = googleTTS.getAudioUrl(q, {
-  lang: 'hi-IN',
+  lang: 'bn-IN',
   slow: false,
   host: 'https://translate.google.com',
 })
