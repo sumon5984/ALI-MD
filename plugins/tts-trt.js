3c5bb48a42b9851b563671b61aeb19ff1f250d2c
sumon5984 <sbot7603@gmail.com>
2025-05-22 18:13:37 +0530

    Update tts-trt.js

--- plugins/tts-trt.js
+++ plugins/tts-trt.js
@@ -44,7 +44,7 @@ cmd({
     pattern: "tts",
     desc: "download songs,t",
     category: "download",
-    react: "",
+    react: "💝",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
